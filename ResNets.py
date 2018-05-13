3699a93c9da01b59cba0cbba26ae8704d762fa1e
Akash <akash.darknight@gmail.com>
2018-05-13 10:03:00 +0530

    Script

--- ResNets.py
+++ ResNets.py
@@ -83,3 +83,30 @@ with tf.Session() as test:
     test.run(tf.global_variables_initializer())
     out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
     print("out = " + str(out[0][1][1][0]))
+
+
+    def convolutional_block(X, f, filters, stage, block, s=2):
+        """
+        Implementation of the convolutional block as defined in Figure 4
+
+        Arguments:
+        X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+        f -- integer, specifying the shape of the middle CONV's window for the main path
+        filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+        stage -- integer, used to name the layers, depending on their position in the network
+        block -- string/character, used to name the layers, depending on their position in the network
+        s -- Integer, specifying the stride to be used
+
+        Returns:
+        X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+        """
+
+        # defining name basis
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+        # Retrieve Filters
+        F1, F2, F3 = filters
+
+        # Save the input value
+        X_shortcut = X
