f0e8f2477990bc3885c048dd31e5aff9691a966a
Akash <akash.darknight@gmail.com>
2018-06-14 13:03:00 +0530

    Script

--- ResNets.py
+++ ResNets.py
@@ -254,7 +254,18 @@ print ("Loss = " + str(preds[0]))
 print ("Test Accuracy = " + str(preds[1]))
 
 model = load_model('ResNet50.h5')
-    
+
 preds = model.evaluate(X_test, Y_test)
 print ("Loss = " + str(preds[0]))
 print ("Test Accuracy = " + str(preds[1]))
+
+img_path = 'images/my_image.jpg'
+img = image.load_img(img_path, target_size=(64, 64))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+print('Input image shape:', x.shape)
+my_image = scipy.misc.imread(img_path)
+imshow(my_image)
+print("class prediction vector [p(0), p(1), p(2), p(3), p(4), p(5)] = ")
+print(model.predict(x))
